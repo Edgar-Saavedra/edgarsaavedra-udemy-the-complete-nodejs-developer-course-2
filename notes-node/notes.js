dc2151540efa2cae8c0de834058d9dc6f5a7ffa1
Edgar Saavedra Vallejo <esaavedra@commonsense.org>
2017-10-14 17:03:15 -0700

    v-10 : custom methods on module

--- notes-node/notes.js
+++ notes-node/notes.js
@@ -1,1 +1,12 @@
-console.log('Starting notes.js')
+console.log('Starting notes.js');
+
+module.exports.age = 30;
+
+module.exports.addNote = () => {
+  console.log('add note');
+  return 'New note';
+};
+
+module.exports.add = (a,b) => {
+  return a+b;
+};
