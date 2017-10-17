e89cf927d1715a0a3cb9a584415ffe59801a20f1
Edgar Saavedra Vallejo <esaavedra@commonsense.org>
2017-10-16 22:18:47 -0700

    v-12 : using nodemon

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,28 +1,7 @@
 console.log('starting app');
 
 const fs = require('fs');
-const os = require('os');
 const _  = require('lodash');
 
-console.log(_.isString(true));
-console.log(_.isString('Edgar'));
-
-var filteredArray = _.uniq(['And','Edgar',1,'Edgar','mike',1,2,3,4]);
-console.log(filteredArray);
-
-
-// const notes = require('./notes.js');
-
-// var user = os.userInfo();
-
-// var res  = notes.addNote();
-// console.log(notes.add(1,2));
-// // console.log(user);
-
-// fs.appendFile('greetings.txt',`\nHello ${user.username}
-// You are ${notes.age}`,function(err) {
-//   if(err) {
-//     console.log('unable to write to file');
-//   }
-// });
-
+//always add space between 3rd party and custom modules
+const notes = require('./notes.js');
--- notes-node/notes.js
+++ notes-node/notes.js
@@ -1,12 +1,6 @@
 console.log('Starting notes.js');
 
-module.exports.age = 30;
-
 module.exports.addNote = () => {
   console.log('add note');
   return 'New note';
-};
-
-module.exports.add = (a,b) => {
-  return a+b;
 };
