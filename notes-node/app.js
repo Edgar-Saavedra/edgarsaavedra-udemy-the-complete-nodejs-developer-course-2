30131d59247bf86e3a9b1a8e43a07e3e4458bdc8
Edgar Saavedra Vallejo <esaavedra@commonsense.org>
2017-10-17 22:20:23 -0700

    v-14 : usings yargs for easier parsing of commandline arguments

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,21 +1,30 @@
-console.log('starting app');
+// console.log('starting app');
 
 const fs = require('fs');
 const _  = require('lodash');
+const yargs = require('yargs');
 
 //always add space between 3rd party and custom modules
 const notes = require('./notes.js');
 
-var command = process.argv[2];
+const argv = yargs.argv;
+var command = process._[0];
+// console.log('Command: ',command);
+// console.log('Process',process.argv);
+// console.log("Yargs",argv);
 
 if( command === 'add' ) {
-  console.log('Adding new note');
+  // console.log('Adding new note');
+  notes.addNote(argv.title,argv.body);
 } else if( command === 'list' ) {
-  console.log('Listing all notes');
+  // console.log('Listing all notes');
+  notes.getAll();
 } else if( command === 'read' ) {
-  console.log('Getting note');
+  // console.log('Getting note');
+  notes.getNote(argv.title);
 } else if( command === 'remove' ) {
-  console.log('Removing note');
+  // console.log('Removing note');
+  notes.removeNote(argv.title);
 } else {
   console.log(`Command, ${command}, not recognized`)
 }
--- notes-node/notes.js
+++ notes-node/notes.js
@@ -1,6 +1,30 @@
-console.log('Starting notes.js');
+// console.log('Starting notes.js');
 
-module.exports.addNote = () => {
-  console.log('add note');
+var addNote = (title,body) => {
+  console.log('Adding note',title,body);
   return 'New note';
 };
+
+var getAll = () => {
+  console.log('Getting all notes');
+};
+
+var getNote = (title) => {
+  console.log('Getting note',title);
+};
+
+var removeNote = (title) => {
+  console.log('Removing note',title);
+};
+
+// module.exports = {
+//   addNote: addNote
+// };
+
+// es6 syntax
+module.exports = {
+  addNote,
+  getAll,
+  getNote,
+  removeNote
+}
